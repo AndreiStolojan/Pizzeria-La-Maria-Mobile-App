944b0261419974d87c1f9f4ac27f518d7ec908fe
Andrei Stolojan <andrei.stolojan@student.upt.ro>
2024-03-30 18:16:17 +0200

    Prima adaugare a proiectului pe acest repository

--- app/App.js
+++ app/App.js
@@ -1,19 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NavigationTab from './Tabs/Navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationTab />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "black",
     alignItems: 'center',
     justifyContent: 'center',
   },
