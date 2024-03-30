e087fae46a8a3e9da8d74d65dc9abd42497db6f7
Andrei Stolojan <andrei.stolojan@student.upt.ro>
2024-03-30 21:10:01 +0200

    Adaugarea tabului pentru crearea profilului in faza initiala

---
+++ app/Tabs/Screens/Login.js
@@ -0,0 +1,78 @@
+// Login.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implementează funcționalitatea de autentificare aici
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Autentificare</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nume de utilizator"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+        placeholderTextColor="#777" // culoare text placeholder
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Parolă"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        placeholderTextColor="#777" // culoare text placeholder
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Autentificare</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    color: 'green',
+    fontWeight: 'bold',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'green',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'green',
+    padding: 15,
+    borderRadius: 20,
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
+export default Login;
