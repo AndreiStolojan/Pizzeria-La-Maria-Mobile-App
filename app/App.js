2606ef0ad2f2a8a84aa1a37eb7f65433537426ea
Andrei Stolojan <andrei.stolojan@student.upt.ro>
2024-05-19 15:31:14 +0300

    pt merge

--- app/App.js
+++ app/App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import NavigationTab from './Tabs/Navigation';
-import { Provider } from 'react-redux'
-import { store } from './store';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import NavigationTab from "./Tabs/RootStack";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
     <Provider store={store}>
-        <NavigationTab />
+      <NavigationTab />
     </Provider>
   );
 }
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black",
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
