50cd953ad8bf6851dc3c1940aee009388d35fe36
Andrei Stolojan <andrei.stolojan@student.upt.ro>
2024-05-27 00:04:38 +0300

    Pagina de rezervare

--- app/App.js
+++ app/App.js
@@ -1,12 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import NavigationTab from './Tabs/Navigation';
-import { useState } from 'react';
+
 import RootStack from './Tabs/Navigators/RootStack';
-import Login from './Tabs/Screens/Login';
-import Profile from './Tabs/Screens/Profil';
-import { CredentialsContext } from './Tabs/Components/CredentialsContext';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
 
